534a3315941b6f3e30c6cc31dcfa3f6ca4514553
itzJunaedRayhan <webgenesis99@gmail.com>
2022-03-06 02:06:49 +0600

    Done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let button = document.getElementById("calculateBtn");
 let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+//  Function to Calculate Age:
 function ageCalculator () {
     let current      = new Date();
     let currentYear  = current.getFullYear();
@@ -14,7 +15,12 @@ function ageCalculator () {
         month : inputDate.getMonth() + 1,
         day   : inputDate.getDate()
     }
-    checkLeapYear(BirthDetails.year);
+
+    
+    //  Call The Function to Check Number of Leap Years between BirthYear and CurrentYear:
+    let NumberOfLeapYears = checkLeapYear(currentYear, BirthDetails.year)
+
+
 
     if (
     BirthDetails.year > currentYear || 
@@ -23,10 +29,12 @@ function ageCalculator () {
     ) {
         alert("Not Born Yet");
         displayResult("-", "-", "-")
-
     } else {
+        //  Get Total Year After Subtract with BirthYear:
         totalYear = currentYear - BirthDetails.year;
         
+
+        //  Get Total Months After Subtract with BirthMonth:
         if (currentMonth >= BirthDetails.month) {
             totalMonth = currentMonth - BirthDetails.month
         } else {
@@ -34,36 +42,37 @@ function ageCalculator () {
             totalMonth = 12 - BirthDetails.month + currentMonth;
         }
 
-        if (currentDay >= BirthDetails.day) {
-            totalDay = currentDay - BirthDetails.day;
+
+        //  Get Total Days After Subtract with BirthDate:
+        if (currentDay >= BirthDetails.day + NumberOfLeapYears) {
+            totalDay = currentDay - BirthDetails.day + NumberOfLeapYears;
         } else {
             totalMonth--;
-            totalDay = months[currentMonth - 2] - BirthDetails.day + currentDay;
+            totalDay = months[currentMonth - 2] - BirthDetails.day + NumberOfLeapYears + currentDay;
         }
 
+
+        //  Calling Function to Display the result to Browser:
         displayResult(totalYear, totalMonth, totalDay);
     }
 }
 
 
 
-
-
-
-//  check Leap Year:
-function checkLeapYear (year) {
-    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
-        months[1] = 29;
-    } else {
-        months[1] = 28;
+//  check Number Of Leap Year:
+function checkLeapYear (current, birthYear) {
+    let count = 0;
+    for (; birthYear <= current; birthYear++) {
+        if (birthYear % 4 == 0 || (birthYear % 100 == 0 && birthYear % 400 == 0)) {
+            count++;
+        }
     }
+    return count;
 }
 
 
 
-
-
-//  display Result
+//  display Result:
 function displayResult (year, month, day) {
     document.querySelector(".years h2").innerHTML = year;
     document.querySelector(".months h2").innerHTML = month;
@@ -72,6 +81,5 @@ function displayResult (year, month, day) {
 
 
 
-
-
+//  Call the age calculator when click on button:
 button.addEventListener("click", ageCalculator)
